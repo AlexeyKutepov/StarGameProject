069271b53051189dbc1e6774c8e16797aa95a3d9
Alexey Kutepov <alkutepov@gmail.com>
2021-05-27 20:29:55 +0300

    Урок 3

--- core/src/ru/gb/screen/MenuScreen.java
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -1,30 +1,30 @@
 package ru.gb.screen;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import ru.gb.base.BaseScreen;
+import ru.gb.math.Rect;
+import ru.gb.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
 
-    private static final float V_LEN= 1.5f;
+    private Texture bg;
 
-    private Texture img;
-    private Vector2 pos;
-    private Vector2 touch;
-    private Vector2 v;
-    private Vector2 tmp;
+    private Background background;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
-        pos = new Vector2();
-        touch = new Vector2();
-        v = new Vector2();
-        tmp = new Vector2();
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
     }
 
     @Override
@@ -32,27 +32,18 @@ public class MenuScreen extends BaseScreen {
         super.render(delta);
         ScreenUtils.clear(0.33f, 0.45f, 0.68f, 1);
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        background.draw(batch);
         batch.end();
-        tmp.set(touch);
-        if (tmp.sub(pos).len() <= v.len()) {
-            pos.set(touch);
-            v.setZero();
-        } else {
-            pos.add(v);
-        }
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        img.dispose();
+        bg.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(touch.cpy().sub(pos)).setLength(V_LEN);
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
         return false;
     }
 }
