dd6ec1cf8471a56fcd5bd75739958b8a15e90fa7
Alexey Kutepov <alkutepov@gmail.com>
2021-05-31 19:12:11 +0300

    ДЗ 3

--- core/src/ru/gb/screen/MenuScreen.java
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -7,32 +7,40 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import ru.gb.base.BaseScreen;
 import ru.gb.math.Rect;
 import ru.gb.sprite.Background;
+import ru.gb.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
+    private Texture badlogic;
 
     private Background background;
+    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
+        badlogic = new Texture("badlogic.jpg");
+        logo = new Logo(badlogic);
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        logo.update(delta);
         ScreenUtils.clear(0.33f, 0.45f, 0.68f, 1);
         batch.begin();
         background.draw(batch);
+        logo.draw(batch);
         batch.end();
     }
 
@@ -40,10 +48,12 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         bg.dispose();
+        badlogic.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        logo.touchDown(touch, pointer, button);
         return false;
     }
 }
