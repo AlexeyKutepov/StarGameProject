c9ea127b2d55a1112dcc734df2f5f3167f3b1b80
Alexey Kutepov <alkutepov@gmail.com>
2021-05-31 20:32:50 +0300

    Урок 4, часть 2

--- core/src/ru/gb/screen/MenuScreen.java
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -1,5 +1,6 @@
 package ru.gb.screen;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
@@ -8,17 +9,27 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import ru.gb.base.BaseScreen;
 import ru.gb.math.Rect;
 import ru.gb.sprite.Background;
+import ru.gb.sprite.ExitButton;
+import ru.gb.sprite.PlayButton;
 import ru.gb.sprite.Star;
 
 public class MenuScreen extends BaseScreen {
 
     private static final int STAR_COUNT = 256;
 
+    private final Game game;
+
     private Texture bg;
     private TextureAtlas atlas;
 
     private Background background;
     private Star[] stars;
+    private ExitButton exitButton;
+    private PlayButton playButton;
+
+    public MenuScreen(Game game) {
+        this.game = game;
+    }
 
     @Override
     public void show() {
@@ -30,6 +41,8 @@ public class MenuScreen extends BaseScreen {
         for (int i = 0; i < stars.length; i++) {
             stars[i] = new Star(atlas);
         }
+        exitButton = new ExitButton(atlas);
+        playButton = new PlayButton(atlas, game);
     }
 
     @Override
@@ -39,6 +52,8 @@ public class MenuScreen extends BaseScreen {
         for (Star star : stars) {
             star.resize(worldBounds);
         }
+        exitButton.resize(worldBounds);
+        playButton.resize(worldBounds);
     }
 
     @Override
@@ -57,6 +72,15 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        exitButton.touchDown(touch, pointer, button);
+        playButton.touchDown(touch, pointer, button);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
+        exitButton.touchUp(touch, pointer, button);
+        playButton.touchUp(touch, pointer, button);
         return false;
     }
 
@@ -73,6 +97,8 @@ public class MenuScreen extends BaseScreen {
         for (Star star : stars) {
             star.draw(batch);
         }
+        exitButton.draw(batch);
+        playButton.draw(batch);
         batch.end();
     }
 }
